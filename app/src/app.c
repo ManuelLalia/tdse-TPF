0c03a4a324fef690bb65381d0e2f7943b8d4a242
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2024-12-16 11:44:03 -0300

    Agregando compilación condicional para el factor de uso y WCET

--- app/src/app.c
+++ app/src/app.c
@@ -44,6 +44,9 @@ const task_cfg_t task_cfg_list[]	= {
 
 #define TASK_QTY	(sizeof(task_cfg_list)/sizeof(task_cfg_t))
 
+#define MOSTRAR_WCET 0
+#define MOSTRAR_FACTOR_USO 0
+
 /********************** internal functions declaration ***********************/
 
 /********************** internal data definition *****************************/
@@ -52,11 +55,20 @@ const char *p_app	= " TPF – Grupo 02\r\n";
 
 /********************** external data declaration ****************************/
 uint32_t g_app_cnt;
-uint32_t g_app_time_us;
-
 volatile uint32_t g_app_tick_cnt;
 
-task_dta_t task_dta_list[TASK_QTY];
+#if MOSTRAR_WCET == 1 || MOSTRAR_FACTOR_USO == 1
+	uint32_t g_app_time_us;
+#endif
+
+#if MOSTRAR_FACTOR_USO == 1
+	uint32_t g_app_time_minimo_us;
+	uint32_t g_app_time_maximo_us;
+#endif
+
+#if MOSTRAR_WCET == 1
+	task_dta_t task_dta_list[TASK_QTY];
+#endif
 
 /********************** external functions definition ************************/
 void app_init(void) {
@@ -78,15 +90,24 @@ void app_init(void) {
 		(*task_cfg_list[index].task_init)(task_cfg_list[index].parameters);
 
 		/* Init variables */
+#if MOSTRAR_WCET == 1
 		task_dta_list[index].WCET = TASK_X_WCET_INI;
+#endif
 	}
+
+#if MOSTRAR_FACTOR_USO == 1
+    	g_app_time_minimo_us = 0xFFFFFFFF;
+    	g_app_time_maximo_us = 0;
+#endif
 
 	cycle_counter_init();
 }
 
 void app_update(void) {
 	uint32_t cycle_counter_time_us;
+#if MOSTRAR_WCET == 1
 	bool empeoro_tiempo = false;
+#endif
 
 	/* Check if it's time to run tasks */
 	if (G_APP_TICK_CNT_INI < g_app_tick_cnt){
@@ -94,27 +115,35 @@ void app_update(void) {
 
     	/* Update App Counter */
     	g_app_cnt++;
+#if MOSTRAR_WCET == 1 || MOSTRAR_FACTOR_USO == 1
     	g_app_time_us = 0;
+#endif
 
     	/* Go through the task arrays */
     	for (uint32_t index = 0; TASK_QTY > index; index++) {
+#if MOSTRAR_WCET == 1 || MOSTRAR_FACTOR_USO == 1
 			cycle_counter_reset();
+#endif
 
     		/* Run task_x_update */
 			(*task_cfg_list[index].task_update)(task_cfg_list[index].parameters);
 
+#if MOSTRAR_WCET == 1 || MOSTRAR_FACTOR_USO == 1
 			cycle_counter_time_us = cycle_counter_time_us();
 
 			/* Update variables */
 	    	g_app_time_us += cycle_counter_time_us;
+#endif
 
+#if MOSTRAR_WCET == 1
 			if (task_dta_list[index].WCET < cycle_counter_time_us) {
 				task_dta_list[index].WCET = cycle_counter_time_us;
 				empeoro_tiempo = true;
 			}
+#endif
 	    }
     }
-
+#if MOSTRAR_WCET == 1
 	if (empeoro_tiempo) {
 		LOGGER_LOG("\nWCET\n----\n");
 		LOGGER_LOG("Task Sensor: %lu us\n", task_dta_list[0].WCET);
@@ -122,9 +151,31 @@ void app_update(void) {
 		LOGGER_LOG("Task Sistema: %lu us\n", task_dta_list[2].WCET);
 		LOGGER_LOG("Task Actuador: %lu us\n", task_dta_list[3].WCET);
 		LOGGER_LOG("Task Display: %lu us\n", task_dta_list[4].WCET);
-
-		LOGGER_LOG("\nFactor Uso: %lu us\n", g_app_time_us);
 	}
+#endif
+
+#if MOSTRAR_FACTOR_USO == 1
+	if (g_app_cnt % 10000 == 0) {
+		g_app_time_minimo_us = 0xFFFFFFFF;
+		g_app_time_maximo_us = 0;
+	}
+
+	bool actualizar = false;
+	if (g_app_time_us < g_app_time_minimo_us) {
+		g_app_time_minimo_us = g_app_time_us;
+		actualizar = true;
+	}
+
+	if (g_app_time_maximo_us < g_app_time_us) {
+		g_app_time_maximo_us = g_app_time_us;
+		actualizar = true;
+	}
+
+	if (actualizar) {
+		LOGGER_LOG("\nFactor Uso Mínimo: %lu us\n", g_app_time_minimo_us);
+		LOGGER_LOG("\nFactor Uso Máximo: %lu us\n", g_app_time_maximo_us);
+	}
+#endif
 }
 
 void HAL_SYSTICK_Callback(void) {
